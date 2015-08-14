6a8a43b4ab949f2d101ad8be453fe0b87cda60ac
Mattias Petter Johansson <mpj@spotify.com>
2015-08-14 20:37:53 +0200

    naming

--- test.js
+++ test.js
@@ -4,11 +4,12 @@ import _ from 'highland'
 import sinon from 'sinon'
 import liar from './liar'
 import 'babel/polyfill'
+import merge from 'mout/object/merge'
 
 test('connection', (t) => {
   t.plan(2)
   let act = makeAct()
-  act.createClient()
+  act.action()
   t.ok(act.connectedWithRightParameters())
   t.ok(act.didSetCorrectEncoding())
 })
@@ -16,7 +17,7 @@ test('connection', (t) => {
 test('write', (t) => {
   t.plan(1)
   let act = makeAct()
-  let str = act.createClient()
+  let str = act.action()
   act.sendMessageToClient()
   act.assertMessageSent(t.pass)
 })
@@ -24,13 +25,13 @@ test('write', (t) => {
 test('read (replay)', (t) => {
   t.plan(2)
   let act = makeAct()
-  act.createClient()
-  act.state.netClient.push('msg hej')
-  act.state.output.assertReceived('hej', t.pass())
-  act.state.netClient.assertReceived(
+  act.action()
+  act.mocks.serverConnection.push('msg hej')
+  act.output.assertReceived('hej', t.pass())
+  act.mocks.serverConnection.assertReceived(
     'consume ' +
-    act.state.topic + ' ' +
-    act.state.generatedUUID.replace(/\-/g,'') + ' ' +
+    act.scene.topic + ' ' +
+    act.scene.generatedUUID.replace(/\-/g,'') + ' ' +
     'smallest\n',
     t.pass
   )
@@ -39,68 +40,82 @@ test('read (replay)', (t) => {
 
 test('read (play)', (t) => {
   t.plan(2)
-  let act = makeAct()
-  act.state.command = 'play'
-  act.createClient()
-  act.state.netClient.push('msg hej')
-  act.state.output.assertReceived('hej', t.pass())
-  act.state.netClient.assertReceived(
+  let act = makeAct({
+    command: 'play'
+  })
+  act.action()
+  act.mocks.serverConnection.push('msg hej')
+  act.output.assertReceived('hej', t.pass())
+  act.mocks.serverConnection.assertReceived(
     'consume ' +
-    act.state.topic + ' ' +
-    act.state.generatedUUID.replace(/\-/g,'') + ' ' +
+    act.scene.topic + ' ' +
+    act.scene.generatedUUID.replace(/\-/g,'') + ' ' +
     'largest\n',
     t.pass
   )
 })
 
-let makeAct = (opts) => {
+let makeAct = (constructorScene) => {
+
   let act = {}
 
-  let state = act.state = {
+  act.scene = merge({
     port: 4444,
     host: '192.168.99.100',
     topic: 'mytopic',
     generatedUUID: '6c84fb90-12c4-11e1-840d-7b25c5ee775a',
     messageToSend: 'whut',
-    command: 'replay',
-    output: liar(),
-    deps: {
-      net: {
-        connect: sinon.stub()
-      },
-      uuid: sinon.stub()
-    },
-    netClient: liar()
-  }
-  state.netClient.setEncoding = sinon.stub()
+    command: 'replay'
+  }, constructorScene)
 
-  act.createClient = () => {
-    state.deps.net.connect
-      .withArgs(state.port, state.host)
-      .returns(state.netClient)
-    state.deps.uuid.returns(state.generatedUUID)
-    state.client = subject(state.deps, {
-      host: state.host,
-      port: state.port
-    }, state.command, state.topic)
-    state.client.pipe(state.output)
-    return state.client
+  act.mocks = {
+    net: {
+      connect: sinon.stub()
+    },
+    uuid: sinon.stub(),
+    serverConnection: merge(liar(), {
+      setEncoding: sinon.stub()
+    })
   }
+
+  act.output = liar()
+
+  act.action = () => {
+
+    act.mocks.net.connect
+      .withArgs(act.scene.port, act.scene.host)
+      .returns(act.mocks.serverConnection)
+
+    act.mocks.uuid.returns(act.scene.generatedUUID)
+
+    act.instance = subject({
+      net: act.mocks.net,
+      uuid: act.mocks.uuid
+    }, {
+      host: act.scene.host,
+      port: act.scene.port
+    }, act.scene.command, act.scene.topic)
+
+    act.instance.pipe(act.output)
+
+    return act.instance
+  }
+
   act.sendMessageToClient = () =>
-    _([state.messageToSend]).pipe(state.client)
+    _([act.scene.messageToSend]).pipe(act.instance)
 
   act.assertMessageSent = (done) =>
-    state.netClient.assertReceived(
-      'send ' + state.topic + ' ' +
-      state.generatedUUID.replace(/\-/g,'') +
-      ' ' + state.messageToSend+'\n',
+    act.mocks.serverConnection.assertReceived(
+      'send ' + act.scene.topic + ' ' +
+      act.scene.generatedUUID.replace(/\-/g,'') +
+      ' ' + act.scene.messageToSend+'\n',
       done)
 
   act.connectedWithRightParameters = () =>
-    state.deps.net.connect.calledWith(state.port, state.host)
+    act.mocks.net.connect.calledWith(act.scene.port, act.scene.host)
 
   act.didSetCorrectEncoding = () =>
-    state.netClient.setEncoding.calledWith('utf8')
+    act.mocks.serverConnection.setEncoding.calledWith('utf8')
 
 
   return act
